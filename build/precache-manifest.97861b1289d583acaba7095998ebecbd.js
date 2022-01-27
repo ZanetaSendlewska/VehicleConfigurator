e785668abae632a4ae300d8017c5fbefd7c66277
Zaneta <z.sendlewska@gmail.com>
2022-01-27 12:29:54 +0100

    master

---
+++ build/precache-manifest.97861b1289d583acaba7095998ebecbd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4f6e438f7f9aa4c462857293dfa2f98",
+    "url": "/VehicleConfigurator/index.html"
+  },
+  {
+    "revision": "33753f55cfb88ceac0ed",
+    "url": "/VehicleConfigurator/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "b9b682180312c66c19b2",
+    "url": "/VehicleConfigurator/static/css/main.3f408fc5.chunk.css"
+  },
+  {
+    "revision": "33753f55cfb88ceac0ed",
+    "url": "/VehicleConfigurator/static/js/2.48204687.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/VehicleConfigurator/static/js/2.48204687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9b682180312c66c19b2",
+    "url": "/VehicleConfigurator/static/js/main.8872f1aa.chunk.js"
+  },
+  {
+    "revision": "b9d52b289cd273b63f36",
+    "url": "/VehicleConfigurator/static/js/runtime-main.d63e380b.js"
+  },
+  {
+    "revision": "25d5bba4753e1ae0c5b2fac703f831ac",
+    "url": "/VehicleConfigurator/static/media/accessories1.25d5bba4.jpg"
+  },
+  {
+    "revision": "b1bb9066208772caa165e8e8d5a0f83e",
+    "url": "/VehicleConfigurator/static/media/accessories2.b1bb9066.jpg"
+  },
+  {
+    "revision": "c504c9bf9c6d11e0072ec13841c485ab",
+    "url": "/VehicleConfigurator/static/media/accessories3.c504c9bf.jpg"
+  },
+  {
+    "revision": "b966343c7bd3b43b69c86bde5f1aaf12",
+    "url": "/VehicleConfigurator/static/media/armchair1.b966343c.jpg"
+  },
+  {
+    "revision": "c2a190a5764df04042f5870a43660447",
+    "url": "/VehicleConfigurator/static/media/armchair2.c2a190a5.jpg"
+  },
+  {
+    "revision": "8d92163325acf01cf01e8c1314d65467",
+    "url": "/VehicleConfigurator/static/media/armchair3.8d921633.jpg"
+  },
+  {
+    "revision": "7cedb00650d12ed3d9f8a1430f121956",
+    "url": "/VehicleConfigurator/static/media/foto1.7cedb006.jpg"
+  },
+  {
+    "revision": "22f3ed92e5753f12aa2798a9f70457cf",
+    "url": "/VehicleConfigurator/static/media/foto2.22f3ed92.jpg"
+  },
+  {
+    "revision": "fbb354cc59b1a68b23cafa2ef34df936",
+    "url": "/VehicleConfigurator/static/media/foto3.fbb354cc.jpg"
+  },
+  {
+    "revision": "84b2ba0d807e7306788a8a04e43abd03",
+    "url": "/VehicleConfigurator/static/media/hoop1.84b2ba0d.jpg"
+  },
+  {
+    "revision": "f351d1202c928ce681eb0e2621d95175",
+    "url": "/VehicleConfigurator/static/media/hoop2.f351d120.jpg"
+  },
+  {
+    "revision": "00dd45b6c7708c6bcdfe6de87c1ae7ff",
+    "url": "/VehicleConfigurator/static/media/hoop3.00dd45b6.jpg"
+  }
+]);
